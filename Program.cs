ccb1de5bafb70a53db1bcd8f3121ed0d44df9a9f
Hilal Sinem Sayar <hilalsinemsayar@gmail.com>
2024-08-07 16:22:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using OpenAI.Managers;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<OpenAICompletionService>();
 builder.Services.AddOpenAIService();
-builder.Services.AddOpenAIService(settings => settings.ApiKey = "sk-_oOF1M-2tJ69jZ1SuUJELtNEGxphFgOEq-9qiimHaHT3BlbkFJ4XeG6WOZxXbO0ymmD43x6NQ-jK1nG7MpbHcay_Ku0A");
+builder.Services.AddOpenAIService(settings => settings.ApiKey = "sk-_oOF1M-2tJ69jZ1SuUJELtNEGxphFgOEq-");
 
 var host = builder.Build();
 host.Run();
